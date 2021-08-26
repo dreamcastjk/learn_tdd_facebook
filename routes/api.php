7ff21503f16df8f930458baf495c9dd980158ddc
Zhiharev <slipdoll@gmail.com>
2021-08-26 11:54:21 +0300

    auth user test and api

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AuthUserController;
 use App\Http\Controllers\Api\UserPostController;
 
 
 
 Route::middleware('auth:api')->group(function () {
+
+    Route::get('auth-user', AuthUserController::class)->name('auth.user');
 
     Route::apiResources([
         'posts' => PostController::class,
