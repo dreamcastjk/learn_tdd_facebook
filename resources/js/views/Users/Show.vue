d7f85c13081325522450b3408409dedd90f7a1a6
Zhiharev <slipdoll@gmail.com>
2021-08-30 16:48:30 +0300

    button for add friend request logic

--- resources/js/views/Users/Show.vue
+++ resources/js/views/Users/Show.vue
@@ -16,7 +16,11 @@
             </div>
 
             <div class="absolute flex items-center bottom-0 right-0 mb-4 mr-12 z-20">
-                <button class="py-1 px-3 bg-gray-400 rounded">Add Friend</button>
+                <button v-if="friendButtonText"
+                        class="py-1 px-3 bg-gray-400 rounded"
+                        @click="$store.dispatch('sendFriendRequest', $route.params.userId)">
+                    {{ friendButtonText }}
+                </button>
             </div>
 
         </div>
@@ -66,6 +70,7 @@ export default {
     computed: {
         ...mapGetters({
             user: 'user',
+            friendButtonText: 'friendButtonText',
         })
     }
 }
