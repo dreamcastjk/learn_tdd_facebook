e69790cd286aad48434c39d487b68c52a67881f8
Zhiharev <slipdoll@gmail.com>
2021-08-30 14:47:13 +0300

    ignore friend request tests and api

--- app/Http/Controllers/Api/FriendRequestResponseController.php
+++ app/Http/Controllers/Api/FriendRequestResponseController.php
@@ -6,6 +6,7 @@ use App\Models\Friend;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\FriendResource;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Requests\IgnoreFriendRequestRequest;
 use App\Exceptions\FriendRequestNotFoundException;
 use App\Http\Requests\FriendRequestResponseRequest;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -30,5 +31,19 @@ class FriendRequestResponseController extends Controller
         $friendRequest->update(array_merge($request->validated(), ['confirmed_at' => now()]));
 
         return new FriendResource($friendRequest);
+    }
+
+    public function destroy(IgnoreFriendRequestRequest $request)
+    {
+        try {
+            Friend::where('user_id', $request['user_id'])
+                ->where('friend_id', auth()->user()->id)
+                ->firstOrFail()
+                ->delete();
+        } catch (ModelNotFoundException $exception) {
+            throw new FriendRequestNotFoundException();
+        }
+
+        return response()->json([], 204);
     }
 }
