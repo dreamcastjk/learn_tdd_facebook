ad248238d8a963e20c8a1621ca6de96ac7c47a1f
Zhiharev <slipdoll@gmail.com>
2021-08-30 17:09:02 +0300

    change friend request button

--- tests/Feature/FriendsTest.php
+++ tests/Feature/FriendsTest.php
@@ -69,7 +69,7 @@ class FriendsTest extends TestCase
     /**
      * @test
      */
-    public function friend_request_can_be_accepted()
+    public function friend_requests_can_be_accepted()
     {
         $this->actingAs($user = User::factory()->create(), 'api');
         $userThatSendFriendRequest = User::factory()->create();
@@ -96,6 +96,8 @@ class FriendsTest extends TestCase
                 'friend_request_id' => $friendRequest->id,
                 'attributes' => [
                     'confirmed_at' => $friendRequest->confirmed_at->diffForHumans(),
+                    'friend_id' => $friendRequest->friend_id,
+                    'user_id' => $friendRequest->user_id,
                 ],
             ],
             'links' => [
